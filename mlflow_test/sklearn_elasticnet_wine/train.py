fbce774d8ee80416bce576652d15ae9b761f0d8e
Dong Nguyen <dong.nguyen@trustingsocial.com>
2019-05-15 15:57:01 +0700

    just a dummy commit

--- mlflow_test/sklearn_elasticnet_wine/train.py
+++ mlflow_test/sklearn_elasticnet_wine/train.py
@@ -2,6 +2,8 @@
 # P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
 # Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
 
+# Just to increase git version
+
 import os
 import warnings
 import sys
